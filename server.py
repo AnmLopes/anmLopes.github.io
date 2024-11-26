419c70e405fa367ef509e12e2fe300d9de179f55
AnmLopes <137410078+AnmLopes@users.noreply.github.com>
2024-11-26 14:33:46 +0000

    server.py

--- server.py
+++ server.py
@@ -1,14 +1,1 @@
-import http.server
-import socketserver
 
-# Set the port number you want to use
-PORT = 8000
-
-# Change to the directory containing your HTML file
-Handler = http.server.SimpleHTTPRequestHandler
-
-# Start the server
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Server started at port", PORT)
-    # This will keep the server running until you press Ctrl+C
-    httpd.serve_forever()
